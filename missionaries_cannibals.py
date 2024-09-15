db269f27e1fd88db1dd7853bae8b0cff0760c8e8
Gordon Pettus <38599393+GLPUSNA67@users.noreply.github.com>
2024-09-15 11:16:54 -0700

    Update missionaries_cannibals.py
    Manual cut and paste update.

--- missionaries_cannibals.py
+++ missionaries_cannibals.py
@@ -1,4 +1,5 @@
 from base_classes import State, Problem
+# Updated 9/15/2024
 
 
 class MCState(State):
@@ -18,38 +19,32 @@ class MCState(State):
         return True
 
     def is_goal(self, goal_state):
-        return self.left_m == 0 and self.left_c == 0
+        return self.left_m == 0 and self.left_c == 0 and not self.boat_left
 
     def __eq__(self, other):
         if not isinstance(other, MCState):
             return False
-        return self.left_m == other.left_m and self.left_c == other.left_c and \
-            self.boat_left == other.boat_left
+        return (self.left_m == other.left_m and
+                self.left_c == other.left_c and
+                self.boat_left == other.boat_left)
 
     def __hash__(self):
         return hash((self.left_m, self.left_c, self.boat_left))
+
+    def __lt__(self, other):
+        return (self.left_m + self.left_c) < (other.left_m + other.left_c)
 
     def __str__(self):
         left_bank = "M" * self.left_m + "C" * self.left_c
         right_bank = "M" * self.right_m + "C" * self.right_c
         boat = "<" if self.boat_left else ">"
-        return "({0}){1}~~~{2}({3})".format(
-            left_bank.ljust(6),
-            boat if self.boat_left else " ",
-            " " if self.boat_left else boat,
-            right_bank.ljust(6)
-        )
-
-    def __lt__(self, other):
-        # Compare based on the number of missionaries and cannibals on the left bank
-        return (self.left_m + self.left_c) < (other.left_m + other.left_c)
+        return f"({''.join(left_bank):6}){boat}~~~{' ' if self.boat_left else boat}({''.join(right_bank):6})"
 
 
 class MCProblem(Problem):
     def __init__(self):
         self._initial_state = MCState(3, 3, True)
         self._goal_state = MCState(0, 0, False)
-        self.moves = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
 
     def get_initial_state(self):
         return self._initial_state
@@ -59,13 +54,22 @@ class MCProblem(Problem):
 
     def get_possible_moves(self, state):
         possible_moves = []
-        for m, c in self.moves:
+        moves = [(1, 0), (2, 0), (0, 1), (0, 2), (1, 1)]
+
+        for m, c in moves:
             if state.boat_left:
                 if m <= state.left_m and c <= state.left_c:
-                    possible_moves.append((m, c, True))
+                    new_state = MCState(
+                        state.left_m - m, state.left_c - c, False)
+                    if new_state.is_valid():
+                        possible_moves.append((m, c, True))
             else:
                 if m <= state.right_m and c <= state.right_c:
-                    possible_moves.append((m, c, False))
+                    new_state = MCState(
+                        state.left_m + m, state.left_c + c, True)
+                    if new_state.is_valid():
+                        possible_moves.append((m, c, False))
+
         return possible_moves
 
     def apply_move(self, state, move):
@@ -78,7 +82,7 @@ class MCProblem(Problem):
     def get_move_description(self, move, state=None):
         m, c, from_left = move
         direction = "from left to right" if from_left else "from right to left"
-        return "{}M {}C {}".format(m, c, direction)
+        return f"{m}M {c}C {direction}"
 
     def heuristic(self, state):
         # Simple heuristic: number of people on the left bank
